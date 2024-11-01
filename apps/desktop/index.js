d71242ef973436036937207dd363578777289c15
huhuanming <huanming@onekey.so>
2024-11-01 18:07:50 +0800

    Update index.js

--- apps/desktop/index.js
+++ apps/desktop/index.js
@@ -1,3 +1,23 @@
+/* eslint-disable import/first */
+/* eslint-disable prefer-rest-params */
+/* eslint-disable no-undef */
+import log from 'electron-log/renderer';
+
+(function () {
+  const oldSetTimeout = globalThis.setTimeout;
+  globalThis.setTimeout = function () {
+    const e = new Error('Just for stack trace');
+    log.info('New timeout registered from %s', e.stack);
+    return oldSetTimeout.apply(this, arguments);
+  };
+  const oldSetInterval = globalThis.setInterval;
+  globalThis.setInterval = function () {
+    const e = new Error('Just for stack trace');
+    log.info('New interval registered from %s', e.stack);
+    return oldSetInterval.apply(this, arguments);
+  };
+})();
+
 import { registerRootComponent } from 'expo';
 
 import App from './App';
