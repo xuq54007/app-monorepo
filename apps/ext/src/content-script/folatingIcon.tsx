c84440f163590229cca082cd1c53430a6ad867ca
huhuanming <huanming@onekey.so>
2024-12-06 12:33:58 +0800

    Update folatingIcon.tsx

--- apps/ext/src/content-script/folatingIcon.tsx
+++ apps/ext/src/content-script/folatingIcon.tsx
@@ -78,66 +78,125 @@ function IconButton({ isExpanded, onClick }: { isExpanded: boolean, onClick: () 
 }
 
 function SecurityInfo({ securityInfo }: { securityInfo: {} }) {
-  return h('div', {
-    style: {
-      display: 'flex',
-      flexDirection: "column",
-      width: '234px',
-      height: '153px',
-      borderTopLeftRadius: '12px',
-      borderBottomLeftRadius: '12px',
-      padding: '8px',
-      background: 'rgba(255, 255, 255, 1)',
-      boxShadow: '0px 8.57px 17.14px 0px rgba(0, 0, 0, 0.09)',
-      transition: 'transform 0.3s ease-in-out',
-    }
-  }, [
-    h('div', {
+
+  return h(
+    "div",
+    {
       style: {
-        display: 'flex',
+        display: "flex",
         flexDirection: "column",
-        gap: '8px'
-      }
-    }, [
-      h('div', {
-        style: {
-          alignItems: 'center',
-          justifyItems: 'space-between'
-        }
-      }, [
-        h('span', {}, 'Uniswap'),
-        h('span', {}, 'x')
-      ]),
-      h('div', {
-        style: {
-          alignItems: 'center',
-          justifyItems: 'space-between'
-        }
-      }, [
-        h('span', {}, 'Uniswap'),
-        h('span', {}, 'x')
-      ]),
-      h('div', {
-        style: {
-          alignItems: 'center',
-          justifyItems: 'space-between'
-        }
-      }, [
-        h('span', {}, 'Uniswap'),
-        h('span', {}, 'x')
-      ]),
-      h('div', {
-        style: {
-          alignItems: 'center',
-          justifyItems: 'space-between'
-        }
-      }, [
-        h('span', {}, 'Uniswap'),
-        h('span', {}, 'x')
-      ])
-    ]),
-    h('div', {}, 'Powered by')
-  ])
+        width: "234px",
+        borderTopLeftRadius: "12px",
+        borderBottomLeftRadius: "12px",
+        padding: "8px 0",
+      },
+    },
+    [
+      h(
+        "div",
+        {
+          style: {
+            display: "flex",
+            flexDirection: "column",
+            gap: "8px",
+          },
+        },
+        [
+          h(
+            "div",
+            {
+              style: {
+                padding: "0 8px",
+                display: 'flex',
+                alignItems: "center",
+                flexDirection: "row",
+                justifyContent: "space-between",
+
+              },
+            },
+            [h("div", {
+              style: {
+                display: 'flex',
+                flexDirection: "row",
+                alignItems: "center",
+                gap: '8px',
+                color: 'rgba(0, 0, 0, 0.88)',
+                fontSize: '13px',
+                fontWeight: '500',
+              },
+            }, [
+              h("img", {
+                src: "https://asset.onekey-asset.com/app-monorepo/bb7a4e71aba56b405faf9278776d57d73b829708/favicon.png",
+                style: {
+                  height: '24px',
+                  width: '24px',
+                },
+              }),
+              "Uniswap",
+            ]), h("div", {
+              width: "24",
+              height: "24",
+              cursor: 'pointer',
+            }, h("svg", {
+              width: "24",
+              height: "24",
+              viewBox: "0 0 24 24",
+              fill: "none",
+              xmlns: "http://www.w3.org/2000/svg"
+            }, [
+              h("path", {
+                "fill-rule": "evenodd",
+                "clip-rule": "evenodd",
+                d: "M7.29289 7.29289C7.68342 6.90237 8.31658 6.90237 8.70711 7.29289L12 10.5858L15.2929 7.29289C15.6834 6.90237 16.3166 6.90237 16.7071 7.29289C17.0976 7.68342 17.0976 8.31658 16.7071 8.70711L13.4142 12L16.7071 15.2929C17.0976 15.6834 17.0976 16.3166 16.7071 16.7071C16.3166 17.0976 15.6834 17.0976 15.2929 16.7071L12 13.4142L8.70711 16.7071C8.31658 17.0976 7.68342 17.0976 7.29289 16.7071C6.90237 16.3166 6.90237 15.6834 7.29289 15.2929L10.5858 12L7.29289 8.70711C6.90237 8.31658 6.90237 7.68342 7.29289 7.29289Z",
+                fill: "#BABABA"
+              })
+            ]))]
+          ),
+          h("div", {
+            style: {
+              background: 'rgba(0, 0, 0, 0.13)',
+              height: '0.33px',
+              width: "100%",
+            }
+          }),
+          h(
+            "div",
+            {
+              style: {
+                alignItems: "center",
+                justifyItems: "space-between",
+              },
+            },
+            [h("span", {}, "Uniswap"), h("span", {}, "x")]
+          ),
+          h(
+            "div",
+            {
+              style: {
+                padding: "0 8px ",
+                alignItems: "center",
+                justifyItems: "space-between",
+              },
+            },
+            [h("span", {}, "Uniswap"), h("span", {}, "x")]
+          ),
+          h(
+            "div",
+            {
+              style: {
+                padding: "0 8px ",
+                alignItems: "center",
+                justifyItems: "space-between",
+              },
+            },
+            [h("span", {}, "Uniswap"), h("span", {}, "x")]
+          ),
+        ]
+      ),
+      h("div", {}, "Powered by"),
+    ]
+  );
+
 }
 
 
